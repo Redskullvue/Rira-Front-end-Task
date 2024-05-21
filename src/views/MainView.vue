b4da520e1ef73efc70c1837e4503a4f4311da070
redskull422 <kofifa.pg@gmail.com>
2024-05-21 21:18:52 +0330

    Update : Renaming Files

---
+++ src/views/MainView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="home">Hello World <i-list /> <i-check /></div>
+</template>
+
+<script setup>
+import IList from "@/components/icons/IList.vue";
+import ICheck from "@/components/icons/ICheck.vue";
+</script>
