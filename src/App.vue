a9f04fee9a84f6ccbe48f56a72531a6dd331df86
redskull422 <kofifa.pg@gmail.com>
2024-05-23 11:23:32 +0330

    Feat : Items are now cached in the local storage

--- src/App.vue
+++ src/App.vue
@@ -13,29 +13,41 @@
 <script setup>
 import "transition-style";
 import YHeader from "@/components/Header.vue";
-import { ref, provide } from "vue";
+import { ref, provide, onMounted } from "vue";
 // This Package is being used to display cool alerts
 import { useToast } from "vue-toastification";
 const toast = useToast();
 
 // Initialized Our main Data here so it would be easier to do provide/inject
-const todosList = ref([
-  { id: 0, title: "Go To Dentist", isDone: false },
-  { id: 1, title: "Make Some food", isDone: false },
-]);
-
+const todosList = ref([]);
 const doneTodos = ref([]);
+
+onMounted(() => {
+  const savedTodos = JSON.parse(localStorage.getItem("todos"));
+  const savedDoneTodos = JSON.parse(localStorage.getItem("doneTodos"));
+
+  if (savedTodos) {
+    todosList.value = savedTodos;
+  }
+  if (savedDoneTodos) {
+    doneTodos.value = savedDoneTodos;
+  }
+});
 
 // This function will fire when an item gets clicked / i passed it with provide function
 function updateTodos(id) {
   let index = todosList.value.findIndex((todo) => todo.id === id);
+  console.log(index);
   let item = todosList.value[index];
+  console.log(item);
 
   if (item.isDone == false) {
     item.isDone = true;
     toast.success("Item Has Been Added To Done List");
     todosList.value.splice(index, 1);
     doneTodos.value.push(item);
+    saveToLocalStorage();
+    saveDoneToLocalStorage();
   } else {
     toast.error("You've Already Done this task");
   }
@@ -46,18 +58,33 @@ function deleteTodo(id) {
   let index = todosList.value.findIndex((todo) => todo.id === id);
   todosList.value.splice(index, 1);
   toast.success("Item Deleted Successfully");
+  saveToLocalStorage();
   return;
 }
 function deleteDoneTodo(id) {
   let index = doneTodos.value.findIndex((todo) => todo.id === id);
   doneTodos.value.splice(index, 1);
+  saveDoneToLocalStorage();
   toast.success("Item Deleted Successfully");
   return;
 }
 
 function addTodo(todo) {
   todosList.value.push(todo);
+  saveToLocalStorage();
 }
+
+// Save To Local Storage
+function saveToLocalStorage() {
+  localStorage.setItem("todos", JSON.stringify(todosList.value));
+}
+
+// Saved Done Tasks in local Storage
+
+function saveDoneToLocalStorage() {
+  localStorage.setItem("doneTodos", JSON.stringify(doneTodos.value));
+}
+
 // Did this instead of prop drilling
 provide("todosList", {
   todosList,
